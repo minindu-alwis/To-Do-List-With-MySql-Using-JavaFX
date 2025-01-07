278fec6cca2b62ed4f792643ae662331806a04b6
minindu-alwis <minindualwis@gmail.com>
2025-01-07 19:15:22 +0530

    todolistnewwwwwww

--- to-do-list-using-mysql/src/main/java/Controller/todolist/ToDoListController.java
+++ to-do-list-using-mysql/src/main/java/Controller/todolist/ToDoListController.java
@@ -1,7 +1,11 @@
 package Controller.todolist;
 
+import Connection.DBConnection;
 import models.ToDoList;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ToDoListController implements ToDoListService{
@@ -20,12 +24,33 @@ public class ToDoListController implements ToDoListService{
 
     @Override
     public boolean addTask(ToDoList task) {
-        return false;
+
+        try {
+            PreparedStatement prepareStm = DBConnection.getInstance().getConnection().prepareStatement("INSERT INTO todolist VALUES(?,?,?,?) ");
+            prepareStm.setString(1, generateTaskId());
+            prepareStm.setString(2, task.getTaskName());
+            prepareStm.setString(3, task.getDate());
+            prepareStm.setString(4, userId);
+            return prepareStm.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
     @Override
     public ArrayList<ToDoList> loadTasks() {
-        return null;
+        ArrayList<ToDoList> todoListArrayList = new ArrayList<>();
+        try {
+            ResultSet rst = DBConnection.getInstance().getConnection().createStatement().executeQuery("SELECT * FROM todolist WHERE user_id='" + userId + "'");
+            while (rst.next()) {
+                todoListArrayList.add(new ToDoList(rst.getString(1), rst.getString(2), rst.getString(3), rst.getString(4)));
+            }
+            return todoListArrayList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -36,7 +61,17 @@ public class ToDoListController implements ToDoListService{
 
     @Override
     public String generateTaskId() {
-        return "";
+        try {
+            ResultSet rst = DBConnection.getInstance().getConnection().createStatement().executeQuery("SELECT task_id FROM todolist ORDER BY task_id DESC LIMIT 1");
+            if (rst.next()) {
+                String existId = rst.getString(1);
+                return String.format("T%04d", Integer.parseInt(existId.substring(1)) + 1);
+            } else {
+                return "T0001";
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
--- to-do-list-using-mysql/src/main/java/Controller/todolist/ToDoListFormController.java
+++ to-do-list-using-mysql/src/main/java/Controller/todolist/ToDoListFormController.java
@@ -1,7 +1,11 @@
 package Controller.todolist;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import models.ToDoList;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,15 +14,47 @@ public class ToDoListFormController implements Initializable {
 
 
     public Label usenametxt;
+    public TextField newTaskNametxt;
+    public DatePicker newTaskDatetxt;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         setName();
+        loadtable();
     }
+
+    private void loadtable() {
+    }
+
 
     private void setName(){
         String name=ToDoListController.getInstance().getuUserName();
         usenametxt.setText(name);
     }
 
+
+    public void btnAddTaskOnAction(ActionEvent actionEvent) {
+        if (newTaskDatetxt.getValue() == null || newTaskNametxt.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Please fill in all fields!").show();
+            return;
+        }
+
+        String taskDate = newTaskDatetxt.getValue().toString();
+
+        boolean taskAdded = ToDoListController.getInstance().addTask(new ToDoList(null, newTaskNametxt.getText(), taskDate, null));
+
+        if (taskAdded) {
+            new Alert(Alert.AlertType.INFORMATION, "Task Added Successfully").show();
+            cleartxts();
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Task Addition Failed").show();
+        }
+
+    }
+
+    private void cleartxts() {
+        newTaskNametxt.clear();
+        newTaskDatetxt.setValue(null);
+    }
 }
